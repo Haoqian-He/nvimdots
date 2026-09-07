c2a2d9cd779a9572819538a7a02846a3305b25b8
Haoqian He <haoqian.he@smartx.com>
2026-09-07 20:11:42 +0800

    feat(telescope): ignore build and generated files

--- lua/modules/configs/tool/telescope.lua
+++ lua/modules/configs/tool/telescope.lua
@@ -21,7 +21,18 @@ return function()
 			path_display = { "absolute" },
 			selection_strategy = "reset",
 			color_devicons = true,
-			file_ignore_patterns = { ".git/", ".cache", "build/", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip" },
+			file_ignore_patterns = {
+				".git/",
+				".cache",
+				"build%d*/",
+				"%.class",
+				"%.pdf",
+				"%.mkv",
+				"%.mp4",
+				"%.zip",
+				"%.pb.h",
+				"%.pb.cc",
+			},
 			layout_config = {
 				horizontal = {
 					preview_width = 0.55,
